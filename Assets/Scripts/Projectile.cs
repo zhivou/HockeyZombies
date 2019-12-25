947ff7d5ee015bb32506b478205adbfa3698a87e
zhivou <zivou4@gmail.com>
2019-12-24 21:22:51 -0700

    Added bullet flow

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public GameObject projectile;
+    public Transform shotPoint;
+    public float timeBetweenShots;
+    private float shotTime;
+
     void Update()
     {
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        //Debug.Log(Input.mousePosition);
-        //Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
         transform.rotation = rotation;
-        
+
+        if(Input.GetMouseButton(0))
+        {
+            if(Time.time > shotTime)
+            {
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                shotTime = Time.time + timeBetweenShots;
+            }
+        }
     }
 }

e9359a9136659a6f8d02282df6b032d3589c2119
zhivou <zivou4@gmail.com>
2019-12-28 15:01:49 -0700

    Added camera settings
    Fixed ratio and scaling, added camera follow functionality

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float cameraSpeed;
+
+    public float minX;
+    public float minY;
+    public float maxX;
+    public float maxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerTransform != null)
+        {
+            float clampedX = Mathf.Clamp(playerTransform.position.x, minX, maxX);
+            float clampedY = Mathf.Clamp(playerTransform.position.y, minY, maxY);
+
+            transform.position = Vector2.Lerp(transform.position, new Vector2(clampedX, clampedY), cameraSpeed);
+        }
+    }
+}

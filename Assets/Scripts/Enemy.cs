b9821e677af8625dc7df8994fe27dfa3b3e2434d
zhivou <zivou4@gmail.com>
2020-02-01 16:49:24 -0700

    Enemy Physics
    Added box colliders to shutgun bulet and Enemies. Setup redgit body for Enemy class.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+
+    public void TakeDamage(int damageAmout)
+    {
+        health -= damageAmout;
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,6 +7,7 @@ public class Projectile : MonoBehaviour
     public float speed;
     public float lifeTime;
     public GameObject explosion;
+    public int damage;
 
     void Start()
     {
@@ -22,5 +23,16 @@ public class Projectile : MonoBehaviour
     {
         Instantiate(explosion, transform.position, Quaternion.identity);
         Destroy(gameObject);
+    }
+
+    // When Box colider colides with another Box colider. collision is an object
+    // Projectile is colidded with. <Enemy> is a class we accessing
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            collision.GetComponent<Enemy>().TakeDamage(damage);
+            DestroyProjectline();
+        }
     }
 }
